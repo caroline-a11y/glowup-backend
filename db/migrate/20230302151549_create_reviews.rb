a62671944e7e02e5b95b81ce6f31bbb780e8ea7a
Caroline Syokau <caroline.syokau@student.moringaschool.com>
2023-03-02 18:57:10 +0300

    created tables

--- db/migrate/20230302151549_create_reviews.rb
+++ db/migrate/20230302151549_create_reviews.rb
@@ -1,4 +1,9 @@
 class CreateReviews < ActiveRecord::Migration[7.0]
   def change
+    create_table :reviews do |t|
+      t.integer :star_rating
+      t.string :comment
+      t.timestamps
+    end
   end
 end
--- db/migrate/20230302151631_add_id_columns.rb.rb
+++ db/migrate/20230302151631_add_id_columns.rb.rb
@@ -1,4 +1,6 @@
-class AddIdColumns.rb < ActiveRecord::Migration[7.0]
+class AddIdColumns < ActiveRecord::Migration[7.0]
   def change
+    add_column :reviews, :user_id, :integer
+    add_column :reviews, :kit_id, :integer
   end
 end
